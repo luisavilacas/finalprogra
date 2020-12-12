4a8e4373d5283e6302a720ec03e4fc8096dd450c
Luis Avila <a2018117998@estudiantes.upsa.edu.bo>
2020-12-12 11:08:03 -0400

    Primera pregunta

---
+++ operacion/operacion/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace operacion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class operacionController : ControllerBase
+
+    {
+        [HttpGet]
+        public string funcion (int numero)
+        {
+            string texto= " " ;
+            if (numero==0)
+            {
+                texto = "Realizado por Luis Avila Castillo";
+
+            }
+            if (numero <0)
+            {
+                texto = "ERROR";
+            }
+            if (numero>0 )
+            {
+                texto = "https://image.freepik.com/vector-gratis/numeros-cartel-imagen_1639-6453.jpg";
+            }
+            return texto;
+        }
+        
+    }
+}
